c5e7f866e4895574f6ab2f33f7328e444f57ded0
Nikolas Haug <nikohaug21@gmail.com>
2020-09-13 21:12:38 -0500

    clean up modal css and js

--- js/modal.js
+++ js/modal.js
@@ -1,44 +1,32 @@
 // get all the modal click  elements in the dom
-// const modalClicks = document.querySelectorAll('.modal-click');
+const modalClicks = document.querySelectorAll('.modal-click');
+const modals = document.querySelectorAll('.modal');
+const modalClose = document.querySelector('.modal__close');
 
-// modalClicks.forEach(modal => {
-//     modal.addEventListener('click', (e) => {
-//         console.log('modal clicked!');
-//         console.log(e.target);
-//         let modalTarget = e.target.getAttribute('data-target');
-//         console.log(modalTarget);
-//         document.getElementById(modalTarget).classList.add('open');
-//         modalOverlay.style.display = 'block';
-//     });
-// });
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var modalClose = document.querySelector('.modal__close');
-
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    // create the modal overlay element
-    modalOverlayElement = `<div class="modal-overlay"></div>`;
-    modal.insertAdjacentHTML('afterend', modalOverlayElement);
-    modal.classList.toggle('open');
-}
+modalClicks.forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        let modalTarget = e.target.getAttribute('data-target');
+        let modalElement = document.getElementById(modalTarget);
+        modalOverlayElement = `<div class="modal-overlay"></div>`;
+        modalElement.insertAdjacentHTML('afterend', modalOverlayElement);
+        modalElement.classList.toggle('open');
+    });
+});
 
 // When the user clicks on <span> (x), close the modal
 modalClose.onclick = function () {
-    modal.classList.toggle('open');
+    modals.forEach(modal => {
+        modal.classList.toggle('open');
+    });
     document.querySelector('.modal-overlay').remove();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (e) {
     if (e.target.classList.contains('modal-overlay')) {
-        modal.classList.toggle('open');
+        modals.forEach(modal => {
+            modal.classList.toggle('open');
+        });
         e.target.remove();         
     }
 }
