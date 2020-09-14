611317ee4539ea32c4e13fa2d93f8d654ac7fd06
Nikolas Haug <nikohaug21@gmail.com>
2020-09-14 11:47:36 -0500

    add profile link in nav

--- auth.js
+++ auth.js
@@ -3,9 +3,93 @@ auth.onAuthStateChanged(user => {
     console.log(user);
     if(user) {
         console.log('user logged in: ', user.email);
+        console.log('display name: ', user.displayName);
         setupUI(user);
     } else {
         console.log('user logged out');
         setupUI();
     }
 });
+
+// get the UI elements from the dom
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const profileLink = document.getElementById('profile');
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+
+        const username = signupForm['signup-username'].value.trim().toString();
+        const user = firebase.auth().currentUser;
+
+        // add the username to the user object
+        return user.updateProfile({
+          displayName: username
+        });
+
+    }).then(() => {
+
+        console.log('user created!');
+        signupForm.reset();
+        document.querySelector('.modal-overlay').remove();
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.classList.remove('open');
+        });
+
+    }).catch(err => {
+        document.querySelector('.error').textContent = err.message;
+    });
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        loginForm.reset();
+        document.querySelector('.modal-overlay').remove();
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.classList.remove('open');
+        });
+    }).catch(err => {
+        loginForm.querySelector('.error').innerHTML = err.message;
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// hide/show UI elements depending on user state
+const setupUI = (user) => {
+    if(user) {
+        // toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+        // display username in nav
+        profileLink.style.display = 'block';
+        profileLink.innerText = user.displayName;
+    } else {
+        // toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+}
